c037d2cf062e3807b8e79543f1dc7ea779466558
natasyaa85 <noviaanggi85@gmail.com>
2025-09-25 10:44:42 +0700

    menambah filr js dan index

---
+++ js/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>SIAP PKL</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <header>
+    <h1>Sistem Informasi PKL</h1>
+  </header>
+
+  <main>
+    <h2>Daftar Siswa</h2>
+    <table>
+      <tr>
+        <th>No</th>
+        <th>Nama</th>
+        <th>Kelas</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Budi</td>
+        <td>XII SIJA 1</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Sari</td>
+        <td>XII SIJA 2</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Andi</td>
+        <td>XII SIJA 3</td>
+      </tr>
+    </table>
+  </main>
+
+  <footer>
+    <p>© 2025 Tim SIJA XII</p>
+  </footer>
+
+  <script src="js/script.js"></script>
+</body>
+</html>
